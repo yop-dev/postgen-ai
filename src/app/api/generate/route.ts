0b3ae4e8ef573383b11a82570c22f19ba1aec091
yop-dev <desilvajoner99@gmail.com>
2026-01-15 18:44:31 +0800

    fix

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -43,7 +43,10 @@ export async function POST(req: Request) {
                         },
                     },
                 },
-                include: { usage: true }
+                include: {
+                    usage: true,
+                    profile: true
+                }
             })
         }
 
